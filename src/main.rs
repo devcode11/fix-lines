7a01beec438f12d3c6498fa3670020f46a4bcee4
devcode11 <80094366+devcode11@users.noreply.github.com>
2025-10-19 16:55:23 +0530

    Handle errors and add a basic test (#1)
    * Improve error messages
    
    * Add clap parser
    
    * Add verbosity flag and basic logging
    
    * Propagate errors from file processing
    
    * Add basic test

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,64 @@
+use anyhow::{Context, Result};
+use clap::Parser;
+use log::info;
+
 #[cfg(windows)]
 const TO_LINE_ENDING: &str = "\r\n";
 #[cfg(not(windows))]
 const TO_LINE_ENDING: &str = "\n";
 
-fn main() {
-    let invoke_name = std::env::args().nth(0).expect("Missing zeroth argument");
-    for arg in std::env::args().skip(1) {
-        if arg == "--help" || arg == "-h" {
-            print_help(invoke_name);
-            return;
-        }
-        process_file(arg);
+/// Fix line endings, remove empty lines at the end of the file and insert a final new line.
+#[derive(Debug, Parser)]
+#[command(version, about, long_about=None)]
+struct CliArgs {
+    /// Files to fix
+    #[arg(required = true)]
+    file_paths: Vec<std::path::PathBuf>,
+
+    #[command(flatten)]
+    verbosity: clap_verbosity_flag::Verbosity,
+}
+
+fn main() -> Result<()> {
+    let args = CliArgs::parse();
+    env_logger::Builder::new()
+        .filter_level(args.verbosity.into())
+        .init();
+
+    info!("{:?}", args);
+
+    for path in args.file_paths {
+        _ = process_file(path.as_path())?;
     }
+    Ok(())
 }
 
-fn print_help(invoke_name: String) {
-    println!(
-"Fix line endings, remove empty lines at the end of the file and insert a final new line.\nusage: {} <file path>...",
-    invoke_name
-    );
+fn process_file(file_path: &std::path::Path) -> Result<()> {
+    info!("Processing file {:?}", file_path);
+    let content = std::fs::read_to_string(file_path)
+        .with_context(|| format!("Failed to read file: {}", file_path.display()))?;
+
+    std::fs::write(file_path, fix_content(content))
+        .with_context(|| format!("Failed to update file: {}", file_path.display()))?;
+    Ok(())
 }
 
-fn process_file(file_path: String) {
-    let read_content = std::fs::read_to_string(file_path.as_str()).expect("Failed to read file");
-    let mut fixed = read_content
+fn fix_content(content: String) -> String {
+    let mut fixed = content
+        .trim_ascii_end()
         .lines()
         .map(|line| line.trim_ascii_end())
         .collect::<Vec<&str>>()
         .join(TO_LINE_ENDING)
-        .trim_ascii_end()
         .to_string();
     fixed.push_str(TO_LINE_ENDING);
+    fixed
+}
 
-    std::fs::write(file_path.as_str(), fixed).expect("Failed to write file");
+#[test]
+fn fix_some_string() {
+    let input = String::from("# some heading\n\na saw ep\n\'spa\ns eqwe q                  qsdqq\n  dqqw e \r asd aaewe\na \nsda\n e            \r\njak\n\nasjalsejlasjea;laksd;a\na xd\nas d\n\n\n\nas dase alkj\n\n\r\n\nasase\n\n\n");
+    let expected = String::from("# some heading\n\na saw ep\n\'spa\ns eqwe q                  qsdqq\n  dqqw e \r asd aaewe\na\nsda\n e\njak\n\nasjalsejlasjea;laksd;a\na xd\nas d\n\n\n\nas dase alkj\n\n\n\nasase\n");
+    let actual = fix_content(input);
+    assert_eq!(actual, expected);
 }
