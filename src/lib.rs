e8c587196b7be481cf616de4b61f47587f995e26
devcode11 <80094366+devcode11@users.noreply.github.com>
2025-10-20 10:07:03 +0530

    Fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -25,8 +25,8 @@ fn fix_string(content: String) -> String {
 
 #[test]
 fn fix_some_string() {
-    let input = String::from("# some heading\n\na saw ep\n\'spa\ns eqwe q                  qsdqq\n  dqqw e \r asd aaewe\na \nsda\n e            \r\njak\n\nasjalsejlasjea;laksd;a\na xd\nas d\n\n\n\nas dase alkj\n\n\r\n\nasase\n\n\n");
-    let expected = String::from("# some heading\n\na saw ep\n\'spa\ns eqwe q                  qsdqq\n  dqqw e \r asd aaewe\na\nsda\n e\njak\n\nasjalsejlasjea;laksd;a\na xd\nas d\n\n\n\nas dase alkj\n\n\n\nasase\n");
+    let input =  "# some heading\n\na saw ep\n\'spa\ns eqwe q                  qsdqq\n  dqqw e \r asd aaewe\na \nsda\n e            \r\njak\n\nasjalsejlasjea;laksd;a\na xd\nas d\n\n\n\nas dase alkj\n\n\r\n\nasase\n\n\n".to_string();
+    let expected = "# some heading\n\na saw ep\n\'spa\ns eqwe q                  qsdqq\n  dqqw e \r asd aaewe\na\nsda\n e\njak\n\nasjalsejlasjea;laksd;a\na xd\nas d\n\n\n\nas dase alkj\n\n\n\nasase\n".to_string();
     let actual = fix_string(input);
     assert_eq!(actual, expected);
 }
